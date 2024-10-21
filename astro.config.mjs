2390dece91695f8f8231cd50fada455e949ce5fe
rezahidayatm <m.rezahidayat2001@gmail.com>
2024-10-21 15:01:48 +0700

    modified:   .github/workflows/astro.yml modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lra.barengreza.xyz',
-    base: 'main',
+    base: '/',
 });
 
